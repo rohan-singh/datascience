73561147f96e97598e3e9bb0c4b21e301c8570a9
Rohan Singh <rohan.vishen@gmail.com>
2016-01-24 22:20:19 +0530

    Create analysis1.R

---
+++ analysis1.R
@@ -0,0 +1,24 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) 
+{
+    #set the path
+    path = directory
+
+    #get the file List in that directory
+    fileList = list.files(path)
+
+    #extract the file names and store as numeric for comparison
+    file.names = as.numeric(sub("\\.csv$","",fileList))
+
+    #select files to be imported based on the user input or default
+    selected.files = fileList[match(id,file.names)]
+
+    #import data
+    Data = lapply(file.path(path,selected.files),read.csv)
+
+    #convert into data frame
+    Data = do.call(rbind.data.frame,Data)
+
+    #calculate mean
+    mean(Data[,pollutant],na.rm=TRUE)
+
+    }
